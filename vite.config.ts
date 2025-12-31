b4fbe58b75ccfa6ae67357a1b43379e167f74d4b
segevo <segevo@gmail.com>
2025-12-31 11:16:59 +0200

    fix2

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // הגדרה חדשה: תמיכה במכשירים ישנים
+      build: {
+        target: 'es2015',
+        outDir: 'dist',
+      },
       server: {
         port: 3000,
         host: '0.0.0.0',
